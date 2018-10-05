17d4a26fdc4006f216f5ad5801cc0acb4673d963
Victor Hugo Barros <hey.zooi@gmail.com>
2018-10-05 00:31:55 -0700

    removing warning

--- sqlite-crypto-vfs.h
+++ sqlite-crypto-vfs.h
@@ -1,8 +1,9 @@
 #ifndef _SQLITE_CRYPTO_VFS_H_
 
 #include <stdlib.h>
+#include <stdint.h>
 
-const char* sqlite_crypto_vfs_name();
+const char* sqlite_crypto_vfs_name(void);
 int sqlite_crypto_vfs_register(const uint8_t key[32], const uint8_t initialization_vector[16], const int make_default);
 
 #endif //_SQLITE_CRYPTO_VFS_H_
